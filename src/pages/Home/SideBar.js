7b7b37f4d020d4142cafb4ac0b59c937a0202efc
tuansyho <tuansyho96@gmail.com>
2020-06-05 10:19:37 +0700

    change Home/SideBar use dom become state

--- src/pages/Home/SideBar.js
+++ src/pages/Home/SideBar.js
@@ -3,26 +3,56 @@ import '../../App.css';
 
 
 class SideBar extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            'slide1': true,
+            'slide2': false,
+            'showSlide11': true,
+            'showSlide22': false
+        }
+        this.showSlide1 = this.showSlide1.bind(this);
+        this.showSlide2 = this.showSlide2.bind(this);
+    }
     showSlide1(){
-        document.getElementsByClassName("active")[0].className = '';
-        document.getElementById("slide1").className = 'active';
-        document.getElementsByClassName("activeButton")[0].className = '';
-        document.getElementById("button1").className = 'activeButton';
+        this.setState({
+            'slide1': true,
+            'slide2': false,
+            'showSlide11': true,
+            'showSlide22': false
+        })
     }
     showSlide2(){
-        document.getElementsByClassName("active")[0].className = '';
-        document.getElementById("slide2").className = 'active';
-        document.getElementsByClassName("activeButton")[0].className = '';
-        document.getElementById("button2").className = 'activeButton';
+        this.setState({
+            'slide1': false,
+            'slide2': true,
+            'showSlide11': false,
+            'showSlide22': true
+        })
     }
-    buttonSlide
     render(){
+        let classNameSlide1 = '';
+        let classNameSlide2 = '';
+        let classNameShowSlide11 = '';
+        let classNameShowSlide22 = '';
+        if(this.state.slide1){
+            classNameSlide1 += 'activeButton';
+        }
+        if(this.state.slide2){
+            classNameSlide2 += 'activeButton';
+        }
+        if(this.state.slide1){
+            classNameShowSlide11 += 'active';
+        }
+        if(this.state.slide2){
+            classNameShowSlide22 += 'active';
+        }
         return(
             <div className="sideBar">
                 <h3 className="title">Pupular Token's Choice</h3>
                 <p className="title">Scure / Stable / Reliable</p>
                 <div className="Slide">
-                    <div className="active chooseSlide" id="slide1"  style={{display: 'none'}}>
+                    <div className={classNameShowSlide11} id="slide1"  style={{display: 'none'}}>
                         <div className="slide slide1 ">
                             <div className="box">
                                 <h3>Biki Mining Pool</h3>
@@ -46,7 +76,7 @@ class SideBar extends Component{
                             </div>
                         </div>
                     </div>
-                    <div className="chooseSlide" id="slide2" style={{display: 'none'}}>
+                    <div className={classNameShowSlide22} id="slide2" style={{display: 'none'}}>
                         <div className="slide slide2">
                             <div className="box">
                                 <h3>Biki Mining Pool</h3>
@@ -69,8 +99,8 @@ class SideBar extends Component{
                     </div>
                 </div>   
                 <div className="buttonChoose">
-                    <button style={{width:'50px', height: '5px'}} className=" activeButton" id="button1" onClick={this.showSlide1}></button>
-                    <button style={{width:'50px', height: '5px'}} className="" id="button2" onClick={this.showSlide2}></button>
+                    <button style={{width:'50px', height: '5px'}} className={classNameSlide1} id="button1" onClick={this.showSlide1}></button>
+                    <button style={{width:'50px', height: '5px'}} className={classNameSlide2} id="button2" onClick={this.showSlide2}></button>
                 </div>             
             </div>
         )
